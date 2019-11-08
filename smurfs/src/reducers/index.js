a58ecb5705c4e3f30a39e3ea29a5209355c78e0b
Brad <brad@viziworx.com>
2019-11-08 13:16:28 -0500

    Create a case for ADD_SMURF_SUCCESS

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,7 +1,10 @@
 import {
     REQUEST_START,
     REQUEST_FAIL,
-    FETCH_SMURFS_SUCCESS
+    FETCH_SMURFS_SUCCESS,
+    ADD_SMURF_START,
+    ADD_SMURF_FAIL,
+    ADD_SMURF_SUCCESS
 } from '../actions'
 
 const initialState = {
@@ -32,6 +35,11 @@ export const reducer = (state = initialState, action) => {
                 isFetching: false,
                 error: '',
                 error: action.payload
+            }
+        case ADD_SMURF_SUCCESS:
+            return {
+                ...state,
+                smurfs: [...action.payload]
             }
         default:
             return state;
