f05cc39ce91cae5b28df817e0557ccb55be366d7
Brad <brad@viziworx.com>
2019-11-08 11:51:18 -0500

    Planned and implemented redux to manage state

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+const store = createStore(reducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>,
+    document.getElementById("root"));
