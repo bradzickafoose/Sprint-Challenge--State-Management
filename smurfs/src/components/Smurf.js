0a6bea457ec25c13477d98513bd1a67b761a5c4d
Brad <brad@viziworx.com>
2020-04-18 20:57:25 -0400

    adds ability to edit smurfs

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,14 +1,33 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { isDeleting } from '../actions';
+import { isEditing, isDeleting } from '../actions';
 
-const Smurf = ({ name, age, height, id, isDeleting }) => {
+const Smurf = ({ name, age, height, id, isEditing, isDeleting }) => {
   const [smurf, setSmurf] = useState({
     name: name,
     age: age,
     height: height,
     id: id
   })
+
+  const [ isEditable, setIsEditable ] = useState(true);
+
+  const handleChange = event => {
+    setSmurf({
+      ...smurf,
+      [ event.target.name ]: event.target.value
+    });
+  }
+
+  const makeEditable = event => {
+    event.preventDefault();
+    setIsEditable(false);
+  }
+
+  const editSmurf = event => {
+    setIsEditable(true);
+    isEditing(smurf);
+  }
 
   const deleteSmurf = event => {
     event.preventDefault();
@@ -17,16 +36,28 @@ const Smurf = ({ name, age, height, id, isDeleting }) => {
 
 
   return (
-    <div className='smurf'>
-    <h2>{smurf.name}</h2>
-    <p>Age: {smurf.age}</p>
-    <p>Height: {smurf.height}</p>
-    <button type='submit' onClick={deleteSmurf}>Delete</button>
-    </div>
+    <form className='smurf'>
+    { isEditable ?
+      <>
+        <h2>{smurf.name}</h2>
+        <p>Age: {smurf.age}</p>
+        <p>Height: {smurf.height}</p>
+        <button onClick={makeEditable}>Edit</button>
+        <button type='submit' onClick={deleteSmurf}>Delete</button>
+      </> :
+      <>
+        <input name='name' value={smurf.name} onChange={handleChange} />
+        <input name='age' value={smurf.age} onChange={handleChange} />
+        <input name='height' value={smurf.height} onChange={handleChange} />
+        <button type='submit' onClick={editSmurf}>Done</button>
+      </>
+    }
+    </form>
   );
 }
 
 const mapDispatchToProps = {
+  isEditing,
   isDeleting
 }
 
