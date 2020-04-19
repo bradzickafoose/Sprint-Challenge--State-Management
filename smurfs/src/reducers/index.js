d4560cde26e3cc273b7b476535755d7dc514bbc7
Brad <brad@viziworx.com>
2020-04-18 20:20:47 -0400

    adds actions to reducer

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,42 +2,51 @@ import {
     FETCH_DATA,
     FETCH_FAILED,
     FETCH_SUCCESS,
-    ADD_SUCCESS
+    ADD_DATA,
+    ADD_SUCCESS,
+    ADD_FAILED,
+    DELETE_DATA,
+    DELETE_SUCCESS,
+    DELETE_FAILED
 } from '../actions'
 
 const initialState = {
     smurfs: [],
-    isFetching: false,
+    isLoading: false,
     error: '',
 }
 
-export const reducer = (state = initialState, action) => {
-    console.log('reducer action:', action)
+export const reducer = (state=initialState, action) => {
+    console.log('reducer state:', state);
+
     switch (action.type) {
         case FETCH_DATA:
+        case ADD_DATA:
+        case DELETE_DATA:
             return {
                 ...state,
-                isFetching: true,
-                error: '',
+                isLoading: true
             }
+
         case FETCH_SUCCESS:
+        case ADD_SUCCESS:
+        case DELETE_SUCCESS:
             return {
                 ...state,
-                isFetching: false,
+                isLoading: false,
                 error: '',
                 smurfs: action.payload
             }
+
         case FETCH_FAILED:
+        case ADD_FAILED:
+        case DELETE_FAILED:
             return {
                 ...state,
-                isFetching: false,
+                isLoading: false,
                 error: action.payload
             }
-        case ADD_SUCCESS:
-            return {
-                ...state,
-                smurfs: [...action.payload]
-            }
+
         default:
             return state;
     }
